fcf1e6eb091987aaf985ed454c9eba9218cf0f0f
Pitti <jhernandezb96@gmail.com>
2019-12-27 15:33:27 -0500

    Vista Orden solo Lectura Terminado (por probar)

--- app/src/RM_Dependiente/java/com/activities/OrdenReadOnlyActivity.java
+++ app/src/RM_Dependiente/java/com/activities/OrdenReadOnlyActivity.java
@@ -6,19 +6,22 @@ import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import com.controllers.OrdenController;
 import com.services.models.ProductoVentaOrdenModel;
+import com.utils.EnvironmentVariables;
 import com.utils.adapters.ProductoVentaOrdenAdapter;
 import com.utils.adapters.ProductoVentaOrdenReadOnlyAdapter;
+import com.utils.exception.ExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class OrdenReadOnlyActivity extends BaseActivity {
 
-    TextView mesa, ordenNo, fecha, dependiente, totalPrincipal, totalSecundaria;
+    TextView mesa, ordenNo, dependiente, totalPrincipal, totalSecundaria;
     ListView listaOrden;
     List<ProductoVentaOrdenModel> productosVOrden;
-    ProductoVentaOrdenReadOnlyAdapter readOnlyAdapter;
+    OrdenController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +37,20 @@ public class OrdenReadOnlyActivity extends BaseActivity {
     protected void initVarialbes() {
         mesa = (TextView) findViewById(R.id.mesaNoLabel);
         ordenNo = (TextView) findViewById(R.id.ordenNoLabel);
-        fecha = (TextView) findViewById(R.id.labelFecha);
         dependiente = (TextView) findViewById(R.id.dependienteLabel);
         totalPrincipal = (TextView) findViewById(R.id.totalPrincipalLabel);
         totalSecundaria = (TextView) findViewById(R.id.totalSecundariaLabel);
         listaOrden = (ListView) findViewById(R.id.listaOrdenReadOnly);
-
+        controller = new OrdenController();
 
         Bundle bundleExtra = getIntent().getExtras();
         mesa.setText(bundleExtra.getString(String.valueOf(R.string.mesa)));//set el No de la mesa
         dependiente.setText(bundleExtra.getString(String.valueOf(R.string.user)));//set el label con el dependiente
         ordenNo.setText(bundleExtra.getString(String.valueOf(R.string.cod_Orden)));//set el label de la orden
 
-        productosVOrden = new ArrayList<ProductoVentaOrdenModel>();
-        readOnlyAdapter = new ProductoVentaOrdenReadOnlyAdapter(getApplicationContext(), R.id.listaOrdenReadOnly, productosVOrden);
+        controller.starService(bundleExtra.getString(String.valueOf(R.string.mesa)),bundleExtra.getString(String.valueOf(R.string.user)));
+        productosVOrden = new ArrayList<ProductoVentaOrdenModel>(controller.getProductoVentaOrden(bundleExtra.getString(String.valueOf(R.string.cod_Orden))));
+        updateCosto();
     }
 
     @Override
@@ -57,6 +60,25 @@ public class OrdenReadOnlyActivity extends BaseActivity {
 
     @Override
     protected void setAdapters() {
+        final ProductoVentaOrdenReadOnlyAdapter readOnlyAdapter = new ProductoVentaOrdenReadOnlyAdapter(this, R.id.listaOrdenReadOnly, productosVOrden);
         listaOrden.setAdapter(readOnlyAdapter);
+    }
+
+    private void updateCosto() {
+        try {
+            float tot = 0;
+            for (ProductoVentaOrdenModel x : productosVOrden) {
+                tot += x.getProductoVentaModel().getPrecioVenta() * x.getCantidad();
+            }
+            if (EnvironmentVariables.MONEDA_PRINCIPAL.equals(EnvironmentVariables.MONEDA_PRINCIPAL)) {
+                totalPrincipal.setText(EnvironmentVariables.setDosLugaresDecimales(tot));
+                totalSecundaria.setText(EnvironmentVariables.setDosLugaresDecimales(tot / EnvironmentVariables.conversion));
+            } else {
+                totalPrincipal.setText(EnvironmentVariables.setDosLugaresDecimales(tot));
+                totalSecundaria.setText(EnvironmentVariables.setDosLugaresDecimales(tot * EnvironmentVariables.conversion));
+            }
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, this);
+        }
     }
 }
