0bcffbf16daf2aac031395ba5a30e8428ed05a3c
Pitti <jhernandezb96@gmail.com>
2020-01-03 11:57:45 -0500

    Arreglado el deLaCasa en el OrdenActivitySoloLectura
    Agregado el check box en el xml

--- app/src/RM_Dependiente/java/com/activities/OrdenReadOnlyActivity.java
+++ app/src/RM_Dependiente/java/com/activities/OrdenReadOnlyActivity.java
@@ -2,6 +2,7 @@ package com.activities;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
@@ -18,19 +19,29 @@ import java.util.List;
 
 public class OrdenReadOnlyActivity extends BaseActivity {
 
-    TextView mesa, ordenNo, dependiente, totalPrincipal, totalSecundaria;
-    ListView listaOrden;
-    List<ProductoVentaOrdenModel> productosVOrden;
-    OrdenController controller;
+    private TextView mesa, ordenNo, dependiente, totalPrincipal, totalSecundaria;
+    private ListView listaOrden;
+    private List<ProductoVentaOrdenModel> productosVOrden;
+    private OrdenController controller;
+    private CheckBox deLaCasaCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orden_readonly);
 
-        initVarialbes();
-        setAdapters();
-        addListeners();
+        try {
+            initVarialbes();
+            setAdapters();
+            addListeners();
+            actDeLaCasa();
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, this);
+        }
+    }
+
+    private void actDeLaCasa() {
+        deLaCasaCheckBox.setChecked(controller.isDeLaCasa());
     }
 
     @Override
@@ -42,13 +53,15 @@ public class OrdenReadOnlyActivity extends BaseActivity {
         totalSecundaria = (TextView) findViewById(R.id.totalSecundariaLabel);
         listaOrden = (ListView) findViewById(R.id.listaOrdenReadOnly);
         controller = new OrdenController();
+        deLaCasaCheckBox = (CheckBox) findViewById(R.id.deLaCasaCheckBoxReadOnly);
+        deLaCasaCheckBox.setEnabled(false);
 
         Bundle bundleExtra = getIntent().getExtras();
         mesa.setText(bundleExtra.getString(String.valueOf(R.string.mesa)));//set el No de la mesa
         dependiente.setText(bundleExtra.getString(String.valueOf(R.string.user)));//set el label con el dependiente
         ordenNo.setText(bundleExtra.getString(String.valueOf(R.string.cod_Orden)));//set el label de la orden
 
-        controller.starService(bundleExtra.getString(String.valueOf(R.string.mesa)),bundleExtra.getString(String.valueOf(R.string.user)));
+        controller.starService(bundleExtra.getString(String.valueOf(R.string.cod_Orden)),bundleExtra.getString(String.valueOf(R.string.mesa)), bundleExtra.getString(String.valueOf(R.string.user)));
         productosVOrden = new ArrayList<ProductoVentaOrdenModel>(controller.getProductoVentaOrden(bundleExtra.getString(String.valueOf(R.string.cod_Orden))));
         updateCosto();
     }
