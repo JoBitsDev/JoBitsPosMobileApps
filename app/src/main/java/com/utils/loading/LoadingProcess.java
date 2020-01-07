3dcc76f402efd664361a1010ea268ba5c1e8165c
Pitti <jhernandezb96@gmail.com>
2020-01-06 23:44:26 -0500

    Creada la clase LoadingProcess
    Creada la clase encargada e contener el procesamiento del metodo lento

---
+++ app/src/main/java/com/utils/loading/LoadingProcess.java
@@ -0,0 +1,14 @@
+package com.utils.loading;
+
+/**
+ * Capa: Utils
+ * Clase que recibe por parametros el LoadingHandler, se sobreescribe el metodo process donde se
+ * va a demorar la aplicacion.
+ */
+public abstract class LoadingProcess {
+    /**
+     * Metodo a demorar de la aplicacion que mientras se ejecute tiene que estar el Dialog de
+     * cargando activado.
+     */
+    public abstract void process() throws Exception;
+}
