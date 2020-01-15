073a462343ed47eea74337be39ab5e657f1ea59a
Pitti <jhernandezb96@gmail.com>
2020-01-15 17:54:29 -0500

    Agregado el bundle nuevamente a BaseAct

--- app/src/main/java/com/activities/BaseActivity.java
+++ app/src/main/java/com/activities/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.activities;
 
 import android.app.*;
+import android.os.Bundle;
 
 /**
  * Capa: Activities
@@ -13,6 +14,8 @@ import android.app.*;
 public abstract class BaseActivity extends Activity {
 
     protected BaseActivity act = this;
+
+    private Bundle bundle;
 
     /**
      * Método abstracto a reimplementar para inicializar las variables del XML en el activity.
@@ -30,6 +33,22 @@ public abstract class BaseActivity extends Activity {
      * Metodo a reimplementar por las activitys que lo necesiten si usan algun adapter.
      */
     protected void setAdapters() {
+    }
+
+    /**
+     * Get el bundle. Es usado por ejemplo en almacen para pasarle cosas al initVariables.
+     * @return
+     */
+    public Bundle getBundle() {
+        return bundle;
+    }
+
+    /**
+     * Set el nuevo bundle.
+     * @param bundle
+     */
+    public void setBundle(Bundle bundle) {
+        this.bundle = bundle;
     }
 
     /**
