1a2bdf89559a95eb5e697aecb9d5eabc86520978
Pitti <jhernandezb96@gmail.com>
2020-01-16 20:50:36 -0500

    Quitado el metodo checkConnection

--- app/src/main/java/com/controllers/BaseController.java
+++ app/src/main/java/com/controllers/BaseController.java
@@ -1,11 +1,6 @@
 package com.controllers;
 
-import java.net.*;
-import java.io.IOException;
-
-import android.os.AsyncTask;
-
-import com.utils.EnvironmentVariables;
+import com.services.web_connections.CheckConnectionWCS;
 
 /**
  * Capa: Controllers
@@ -16,11 +11,6 @@ import com.utils.EnvironmentVariables;
 public abstract class BaseController {
 
     /**
-     * URL con la coneccion al servidor.
-     */
-    protected final String URLCONN = "http://" + EnvironmentVariables.IP + ":" + EnvironmentVariables.PORT + "/RM";
-
-    /**
      * Chequea la coneccion con el servidor, hace un ping y verifica respuesta.
      * En caso de CUALQUIER error con el servido lo maneja y da false.
      *
@@ -28,30 +18,9 @@ public abstract class BaseController {
      */
     public boolean checkConnection() {
         try {
-            return checkConnection(URLCONN);
+            return new CheckConnectionWCS().checkConnection();
         } catch (Exception e) {
             return false;
         }
-    }
-
-    protected Boolean checkConnection(String uri) {
-        URL url = null;
-        try {
-            url = new URL(uri);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setDoInput(true);
-            // Starts the query
-            con.setConnectTimeout(500);//tiempo de espera maximo de la coneccion
-            con.connect();
-            return true;
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return false;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }//TODO: arreglar esto que no pincha bien
-
     }
 }
