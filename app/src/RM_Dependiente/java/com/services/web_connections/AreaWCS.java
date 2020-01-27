2890ea637102f49c8da69e71954786cec1a32091
Pitti <jhernandezb96@gmail.com>
2020-01-26 21:51:27 -0500

    Agregado todos los FIND mesas

--- app/src/RM_Dependiente/java/com/services/web_connections/AreaWCS.java
+++ app/src/RM_Dependiente/java/com/services/web_connections/AreaWCS.java
@@ -1,5 +1,9 @@
 package com.services.web_connections;
 
+import com.services.models.MesaModel;
+
+import java.util.List;
+
 /**
  * Created by Jorge on 2/7/17.
  */
@@ -10,7 +14,9 @@ public class AreaWCS extends SimpleWebConnectionService {
     /**
      * Etiquetas a llamar.
      */
-    final String AREAS = "AREAS";
+    final String FIND_VACIAS = "FIND_VACIAS",
+            FIND_ALL = "FIND-ALL",
+            FIND_ALL_MESAS_AREA = "FIND-ALL-MESAS-AREA";
 
     public AreaWCS() {
         super();
@@ -21,4 +27,22 @@ public class AreaWCS extends SimpleWebConnectionService {
         String resp = connect(path, null, super.TOKEN, HTTPMethod.GET);
         return om.readValue(resp, om.getTypeFactory().constructArrayType(String.class));
     }
+
+    public String[] findVacias(String codMesa) throws Exception {
+        String URL = path + FIND_VACIAS + "?codMesa=" + codMesa;
+        String resp = connect(URL, null, super.TOKEN, HTTPMethod.GET);
+        return om.readValue(resp, om.getTypeFactory().constructArrayType(String.class));
+    }
+
+    public List<MesaModel> findMesas() throws Exception {
+        String resp = connect(path + FIND_ALL, null, super.TOKEN, HTTPMethod.GET);
+        return om.readValue(resp, om.getTypeFactory().constructCollectionType(List.class, MesaModel.class));
+    }
+
+    public List<MesaModel> findMesas(String selectedArea) throws Exception {
+        String URL = path + FIND_ALL_MESAS_AREA + "?selectedArea=" + selectedArea;
+        String resp = connect(URL, null, super.TOKEN, HTTPMethod.GET);
+        return om.readValue(resp, om.getTypeFactory().constructCollectionType(List.class, MesaModel.class));
+    }
+
 }
