14aa2fe0f90e4aa1af1d231bab63c9f71470e295
Jorge Hurtado <jjhurtado796@gmail.com>
2020-12-26 09:09:08 -0500

    Refactorizacion de fecha de ventas por idVenta

--- app/src/JoBits_POS_Manager/java/com/controllers/ResumenVentasController.java
+++ app/src/JoBits_POS_Manager/java/com/controllers/ResumenVentasController.java
@@ -3,7 +3,6 @@ package com.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.services.models.DetallesVentasModel;
 import com.services.models.VentaResumenModel;
-import com.services.web_connections.HTTPMethod;
 import com.services.web_connections.ResumenVentasWCS;
 
 import java.util.List;
@@ -11,24 +10,30 @@ import java.util.List;
 public class ResumenVentasController extends BaseController {
     private ResumenVentasWCS resumenVentasWCS = new ResumenVentasWCS();
 
-    public VentaResumenModel getResumenVentas(String fecha) throws Exception {
-        String body = resumenVentasWCS.getResumenVentas(fecha);
+
+    public List<Integer> getResumenVentasCount(String fecha) throws Exception {
+        return resumenVentasWCS.getResumenVentasCount(fecha);
+
+    }
+
+    public VentaResumenModel getResumenVentas(int idVenta) throws Exception {
+        String body = resumenVentasWCS.getResumenVentas(idVenta);
         return new ObjectMapper().readValue(body, VentaResumenModel.class);
     }
 
-    public List<DetallesVentasModel> getDetallesPorArea(String fecha, String areaCod) throws Exception {
-        return resumenVentasWCS.getDetallesPorArea(fecha, areaCod);
+    public List<DetallesVentasModel> getDetallesPorArea(int idVenta, String areaCod) throws Exception {
+        return resumenVentasWCS.getDetallesPorArea(idVenta, areaCod);
     }
 
-    public List<DetallesVentasModel> getDetallesPorDependientes(String fecha, String usuario) throws Exception {
-        return resumenVentasWCS.getDetallesPorDependientes(fecha, usuario);
+    public List<DetallesVentasModel> getDetallesPorDependientes(int idVenta, String usuario) throws Exception {
+        return resumenVentasWCS.getDetallesPorDependientes(idVenta, usuario);
     }
 
-    public List<DetallesVentasModel> getDetallesPorCocina(String fecha, String cocinaCod) throws Exception {
-        return resumenVentasWCS.getDetallesPorCocina(fecha, cocinaCod);
+    public List<DetallesVentasModel> getDetallesPorCocina(int idVenta, String cocinaCod) throws Exception {
+        return resumenVentasWCS.getDetallesPorCocina(idVenta, cocinaCod);
     }
 
-    public List<DetallesVentasModel> getDetallesPor(String fecha) throws Exception {
-        return resumenVentasWCS.getDetallesPor(fecha);
+    public List<DetallesVentasModel> getDetallesPor(int idVenta) throws Exception {
+        return resumenVentasWCS.getDetallesPor(idVenta);
     }
 }
--- app/src/JoBits_POS_Manager/java/com/services/web_connections/ResumenVentasWCS.java
+++ app/src/JoBits_POS_Manager/java/com/services/web_connections/ResumenVentasWCS.java
@@ -8,41 +8,49 @@ public class ResumenVentasWCS extends SimpleWebConnectionService {
 
     private final String p = "venta/",
             RESUMEN = "SALES",
+            RESUMEN_COUNT = "SALES-COUNT",
             DETALLES_POR_AREA = "DETALLES-POR-AREA",
             DETALLES_POR_DEPENDIENTE = "DETALLES-POR-DEPENDIENTE",
             DETALLES_POR_COCINA = "DETALLES-POR-COCINA",
             DETALLES_POR = "DETALLES-POR";
+
 
     public ResumenVentasWCS() {
         super();
         path += p;
     }
 
-    public String getResumenVentas(String fecha) throws Exception {
-        String URL = path + RESUMEN + "?fecha=" + fecha.replace("/", "-");
+    public List<Integer> getResumenVentasCount(String fecha) throws Exception {
+        String URL = path + RESUMEN_COUNT + "?fecha=" + fecha.replace("/", "-");
+        String resp = connect(URL, null, TOKEN, HTTPMethod.GET);
+        return om.readValue(resp, om.getTypeFactory().constructCollectionType(List.class, Integer.TYPE));
+    }
+
+    public String getResumenVentas(int idVenta) throws Exception {
+        String URL = path + RESUMEN + "?idVenta=" + idVenta;
         return connect(URL, null, TOKEN, HTTPMethod.GET);
     }
 
-    public List<DetallesVentasModel> getDetallesPorArea(String fecha, String areaCod) throws Exception {
-        String URL = path + DETALLES_POR_AREA + "?fecha=" + fecha.replace("/", "-") + "&areaCod=" + areaCod;
+    public List<DetallesVentasModel> getDetallesPorArea(int idVenta, String areaCod) throws Exception {
+        String URL = path + DETALLES_POR_AREA + "?idVenta=" + idVenta + "&areaCod=" + areaCod;
         String resp = connect(URL, null, TOKEN, HTTPMethod.GET);
         return om.readValue(resp, om.getTypeFactory().constructCollectionType(List.class, DetallesVentasModel.class));
     }
 
-    public List<DetallesVentasModel> getDetallesPorDependientes(String fecha, String usuario) throws Exception {
-        String URL = path + DETALLES_POR_DEPENDIENTE + "?fecha=" + fecha.replace("/", "-") + "&usuario=" + usuario;
+    public List<DetallesVentasModel> getDetallesPorDependientes(int idVenta, String usuario) throws Exception {
+        String URL = path + DETALLES_POR_DEPENDIENTE + "?idVenta=" + idVenta + "&usuario=" + usuario;
         String resp = connect(URL, null, TOKEN, HTTPMethod.GET);
         return om.readValue(resp, om.getTypeFactory().constructCollectionType(List.class, DetallesVentasModel.class));
     }
 
-    public List<DetallesVentasModel> getDetallesPorCocina(String fecha, String cocinaCod) throws Exception {
-        String URL = path + DETALLES_POR_COCINA + "?fecha=" + fecha.replace("/", "-") + "&cocinaCod=" + cocinaCod;
+    public List<DetallesVentasModel> getDetallesPorCocina(int idVenta, String cocinaCod) throws Exception {
+        String URL = path + DETALLES_POR_COCINA + "?idVenta=" + idVenta + "&cocinaCod=" + cocinaCod;
         String resp = connect(URL, null, TOKEN, HTTPMethod.GET);
         return om.readValue(resp, om.getTypeFactory().constructCollectionType(List.class, DetallesVentasModel.class));
     }
 
-    public List<DetallesVentasModel> getDetallesPor(String fecha) throws Exception {
-        String URL = path + DETALLES_POR + "?fecha=" + fecha.replace("/", "-");
+    public List<DetallesVentasModel> getDetallesPor(int idVenta) throws Exception {
+        String URL = path + DETALLES_POR + "?idVenta=" + idVenta;
         String resp = connect(URL, null, TOKEN, HTTPMethod.GET);
         return om.readValue(resp, om.getTypeFactory().constructCollectionType(List.class, DetallesVentasModel.class));
     }
