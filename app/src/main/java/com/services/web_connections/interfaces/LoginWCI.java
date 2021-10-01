c63f73187369bbe6e0d38ca15a8186bdb68964e3
Jorge Hurtado <jjhurtado796@gmail.com>
2021-10-01 12:40:45 -0400

    Interfaces Created

---
+++ app/src/main/java/com/services/web_connections/interfaces/LoginWCI.java
@@ -0,0 +1,21 @@
+package com.services.web_connections.interfaces;
+
+import com.services.models.Token;
+
+import java.util.Map;
+import java.util.Set;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
+public interface LoginWCI {
+
+    @GET("pos/auth/basic")
+    Call<Map<String,String>> getToken(@Header("Tennant") String tennantToken, @Header("Authorization") String base64Credentials);
+
+    @GET("tennant/cuenta/{idCuenta}/token-for/{idBaseDatos}")
+    Call<Token> getTennantToken(@Header("Authorization") String base64CredentialsForTennant,
+                                 @Path("idCuenta") int idCuenta, @Path("idBaseDatos") int idBaseDatos);
+}
